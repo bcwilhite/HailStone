f74b2d97733897f0faed1a53d67f18de6425be51
Brian Wilhite <bcwilhite@live.com>
2019-08-12 23:03:14 -0400

    Initial Commit

---
+++ HailStoneSequence.psm1
@@ -0,0 +1,67 @@
+function Invoke-EvenNumberRoutine
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [Int32]
+        $Number
+    )
+
+    return $Number / 2
+}
+
+function Invoke-OddNumberRoutine
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [Int32]
+        $Number
+    )
+
+    return ($Number * 3) + 1
+}
+
+<#
+    .SYNOPSIS
+        Returns the HailStone Sequence
+    .DESCRIPTION
+        Returns the HailStone Number Sequence, or Collatz conjecture.  Wikipedia definition:
+        Start with any positive integer n. Then each term is obtained from the previous term as follows:
+        if the previous term is even, the next term is one half the previous term.
+        If the previous term is odd, the next term is 3 times the previous term plus 1.
+        The conjecture is that no matter what value of n, the sequence will always reach 1.
+    .PARAMETER StartingNumber
+        Any positive interger to start the HailStone / Collatz conjecture.
+#>
+function Get-HailStoneSequence
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [Int32]
+        $StartingNumber
+    )
+
+    $number = $StartingNumber
+    while ($number -ne 1)
+    {
+        switch ($number % 2)
+        {
+            0 {
+                $number = Invoke-EvenNumberRoutine -Number $number
+            }
+
+            1 {
+                $number = Invoke-OddNumberRoutine -Number $number
+            }
+        }
+
+        $number
+    }
+}
+
+Export-ModuleMember -Function '*-HailStoneSequence'
